c5bc8193675a1557fd9df0a043e92ee9ffd0f7ea
Emanuel Antônio Rocha <rochaemanuel733@gmail.com>
2025-10-27 22:09:03 -0300

    upload de imagens

--- frontend/1_loginsession/pagina1.php
+++ frontend/1_loginsession/pagina1.php
@@ -1,3 +1,30 @@
 <?php
-    echo 'Mariposa do nordeste';
+    session_start();
+
+if (isset($_SESSION['login']))
+{
+    $mensagem = 'Seja bem vindo ' . $_SESSION['login'] . '! </br> Neste momento voc&ecirc; est&aacute; logado em nosso sistema e na p&aacute;gina 1. </br>
+    <a href="logout.php">SAIR DO SISTEMA</a></br>';
+}
+else
+{
+    $mensagem = 'Essa p&aacute;gina &eacute; de acesso restrito! </br>
+    <a href="index.php">VOLTAR</a></br>';
+}
 ?>
+<html>
+    <head></head>
+    <body>
+        <center>
+            <h2>
+                <?php
+                    echo $mensagem;
+                ?>
+            </h2>
+            </br></br>
+            <a href="pagina1.php">P&aacute;gina 1</a>
+            <a href="pagina2.php">P&aacute;gina 2</a>
+            <a href="pagina3.php">P&aacute;gina 3</a>
+        </center>
+    </body>
+</html>
